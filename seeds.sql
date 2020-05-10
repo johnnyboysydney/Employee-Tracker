a7d7386dcd1d0f330b27aaf322c9ad495eeeaf05
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-10 11:16:26 +1000

    Adding DB content

--- seeds.sql
+++ seeds.sql
@@ -11,13 +11,13 @@ VALUES
 INSERT INTO role (id, title, salary, department_id)
 VALUES 
 (1, "CEO", 120000, 1), 
-(2, "Product Manager", 80000, 1), 
+(2, "Product Manager", 90000, 1), 
 (3, "Senior Engineer", 80000, 2),
-(4, "Junior Engineer", 80000, 2),
-(5, "Sales Lead", 80000, 3),
+(4, "Junior Engineer", 70000, 2),
+(5, "Sales Lead", 75000, 3),
 (6, "Creative Director", 80000, 4),
 (7, "Social Media Manager", 60000, 4),
-(8, "Chief Quality Tester", 110000, 1),
+(8, "Chief Quality Tester", 90000, 1),
 (9, "Tester", 65000, 2);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
