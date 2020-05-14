bb9755f68ae387b305b94ffc86f6e29451b59470
Johnny <54227198+johnnyboysydney@users.noreply.github.com>
2020-05-14 21:57:25 +1000

    Update server.js
    Removing password

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Terror301",
+    password: "",
     database: "employeesdb"
   });
 
